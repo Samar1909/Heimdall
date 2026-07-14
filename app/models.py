37f90795112c5437478dde3e8814d1aa42e37372
Samar1909 <simarjot1909@gmail.com>
2026-07-14 16:18:22 +0530

    Added Schema

--- app/models.py
+++ app/models.py
@@ -1,4 +1,37 @@
 from pydantic import BaseModel
+from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename = 'users'
+    
+    user_id = Column(String(50), primary_key=True, index=True)
+    dob_year = Column(Integer)
+    gender = Column(Integer)
+    city_pop = Column(Integer)
+    job = Column(String(100))
+    state = Column(String(50))
+    lat = Column(Float)
+    long = Column(Float)
+    
+class Merchant(Base):
+    __tablename__ = "merchants"
+
+    merchant_id = Column(String(50), primary_key=True, index=True)
+    category = Column(String(100))
+    lat = Column(Float)
+    long = Column(Float)
+    
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    transaction_id = Column(String(100), primary_key=True, index=True)
+    user_id = Column(String(50), ForeignKey("users.user_id"))
+    merchant_id = Column(String(50), ForeignKey("merchants.merchant_id"))
+    amt = Column(Float)
+    unix_time = Column(Integer)
+    fraud_probability = Column(Float)
+    status = Column(String(50))
 
 class TransactionPayload(BaseModel):
     amt: float
